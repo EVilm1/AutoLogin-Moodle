14252fc5ee434add6172afa198f29da98a46e518
EVilm¹ <51506790+EVilm1@users.noreply.github.com>
2023-03-10 00:27:47 +0100

    Update autologin.js
    Bugs fixes.

--- autologin.js
+++ autologin.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         ICAM Moodle auto login
-// @name:he      auto
-// @namespace    http://tampermonkey.net/
-// @version      1
-// @description  An automatic login script for moodle.
+// @name         Moodle auto login for ICAM
+// @name:fr      Moodle auto login for ICAM
+// @version      1.2
+// @description  An automatic login script for Moodle.
+// @description:fr  Un login automatique pour Moodle
 // @author       EVilm1
 // @match        https://cas.icam.fr/cas/login
 // @match        https://moodle.icam.fr/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @license MIT
 // ==/UserScript==
-
 
 (function() {
     'use strict';
@@ -20,23 +20,27 @@
         password: "$K*k66T5",
     }
 
-    function login(){
+    function login() {
         document.querySelector("input#username").value = loginDetails.userName;
         document.querySelector("input#password").value = loginDetails.password;
-
-        setTimeout(clickLoginButton, 300);
+        document.querySelector("button#defaultSubmitBtn").removeAttribute("disabled");
+        setTimeout(clickLoginButton, 200);
     }
 
-    function clickLoginButton(){
-        document.querySelector("button#defaultSubmitBtn").click()
+    function clickLoginButton() {
+        document.querySelector("button#defaultSubmitBtn").click();
     }
 
     function isLoggedIn() {
-        return document.getElementsByClassName('usertext').length > 0
+        return document.getElementsByClassName('usertext').length > 0 // 0 = not logged
     }
 
     function redirectToLogin() {
         window.location.href = 'https://cas.icam.fr/cas/login';
+    }
+
+    function redirectToMoodle() {
+        window.location.href = 'https://moodle.icam.fr/login/index.php?authCAS=CAS';
     }
 
     var host = window.location.host;
@@ -47,15 +51,22 @@
             redirectToLogin();
         }
     } else if (host == "cas.icam.fr") {
+        addEventListener('load', (event) => {});
         window.addEventListener('load', (event) => {
             // wait for 'load' to allow reacting faster without setTimeOut
-            login()
+            console.log("logging in...")
+            setTimeout(login(), 200);
         });
         window.addEventListener('load', (event) => {
             // wait for 'load' to allow reacting faster without setTimeOut
-            $('a[href*="https://moodle.icam.fr/login/index.php?authCAS=CAS"]')[0].click()
-            //location.href = "https://moodle.icam.fr/login/index.php?authCAS=CAS";
+            console.log("redirection Moodle...")
+            setTimeout(redirectToMoodle(), 200);
+        });
+    }
 
-        });
+    if (host == "cas.icam.fr") {
+        if(document.title == "Connexion réussie - Icam – Central Authentication Service"){
+            redirectToMoodle();
+        }
     }
 })();
